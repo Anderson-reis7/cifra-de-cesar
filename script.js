8017067abff80f8c117e59b6cc0be8b65c4ceb9a
Anderson-reis7 <Anderson.200810@gmail.com>
2024-10-03 19:32:22 -0300

    Implementado a logica com javascript e finalizando o projeto

--- script.js
+++ script.js
@@ -28,8 +28,45 @@ const alfabeto = [
 ];
 
 const seletor = document.getElementById("deslocamento");
+const texto = document.getElementById("para-criptografar");
+const botao = document.getElementById("botao");
+const resposta = document.getElementById("resposta");
 
 for (let i = 0; i < alfabeto.length; ++i) {
   seletor.innerHTML =
     seletor.innerHTML + `<option value="${i}">${alfabeto[i]}</option>`;
+}
+
+botao.addEventListener("click", () => {
+  let textoParaCriptografar = texto.value;
+  let deslocamento = +seletor.value;
+  let cifrado = cifrar(textoParaCriptografar, deslocamento);
+  resposta.classList.remove("invisivel");
+  resposta.innerHTML = cifrado;
+});
+
+function cifrar(texto, deslocamento) {
+  let textoMaiuscula = texto.toUpperCase().split("");
+  let textoCriptografado = [];
+
+  for (let i = 0; i < textoMaiuscula.length; i++) {
+    let indiceDaLetra = alfabeto.indexOf(textoMaiuscula[i]);
+    if (indiceDaLetra >= 0) {
+      textoCriptografado.push(letraPorIndice(indiceDaLetra + deslocamento));
+    } else {
+      textoCriptografado.push(textoMaiuscula[i]);
+    }
+  }
+
+  return textoCriptografado.join("");
+}
+
+function letraPorIndice(indice) {
+  let indiceFinal;
+  if (indice >= 0) {
+    indiceFinal = indice % alfabeto.length;
+  } else {
+    indiceFinal = alfabeto.length + (indice % alfabeto.length);
+  }
+  return alfabeto[indiceFinal];
 }
